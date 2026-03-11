1801fa9342d50e6ae4a93e66ff60d73de0319296
Lu69as <lukasokken@gmail.com>
2026-03-11 14:32:18 +0100

    Actually finished frontpage

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-window.addEventListener("load", () => {
-    let introSpans = document.querySelectorAll("#home h1 span");
-    let currentintroSpans = 0;
+// --------------------------------------------------------------------------------------- //
+// Import navbar and footer from files and create link hover effect                       //
+// ------------------------------------------------------------------------------------- //
+{
+    fetch('/src/navbar.html')
+      .then(response => { return response.text() })
+      .then(html => document.querySelectorAll("nav").forEach((e) => e.innerHTML = html)
+    );
+    fetch('/src/footer.html')
+      .then(response => { return response.text() })
+      .then(html => document.querySelectorAll("footer").forEach((e) => e.innerHTML = html)
+    );
+}
 
-    function cycleHeaderAppName() {
-        introSpans.forEach(e => e.classList.remove("titleAppNamesAni"));
-        introSpans[currentintroSpans].classList.add("titleAppNamesAni");
-        currentintroSpans = (currentintroSpans + 1) % introSpans.length;
+// --------------------------------------------------------------------------------------- //
+// All code happening after load                                                          //
+// ------------------------------------------------------------------------------------- //
+window.addEventListener("load", () => {
+    // ----------------------------------------------------- //
+    // Go through app names in home page header             //
+    // --------------------------------------------------- //
+    {
+        let introSpans = document.querySelectorAll("#home h1 span");
+        let currentintroSpans = 0;
+    
+        function cycleHeaderAppName() {
+            introSpans.forEach(e => e.classList.remove("titleAppNamesAni"));
+            introSpans[currentintroSpans].classList.add("titleAppNamesAni");
+            currentintroSpans = (currentintroSpans + 1) % introSpans.length;
+        }
+        cycleHeaderAppName();
+        setInterval(cycleHeaderAppName, 3000);
     }
-    cycleHeaderAppName();
-    setInterval(cycleHeaderAppName, 3000);
 })
 
+
+// --------------------------------------------------------------------------------------- //
+// Code firing when scrolling                                                             //
+// ------------------------------------------------------------------------------------- //
 window.addEventListener("scroll", () => {
     if (window.scrollY > 0) document.querySelector("nav").classList.add("detachedNav");
     else document.querySelector("nav").classList.remove("detachedNav");
