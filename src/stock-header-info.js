6ab3837fa990c64f7ab7112dbcebfaa10e12c448
Lu69as <lukasokken@gmail.com>
2026-04-24 12:22:23 +0200

    Some SEO fixes

---
+++ src/stock-header-info.js
@@ -0,0 +1,17 @@
+document.querySelector("script#stock_header_info_replaceByCode").outerHTML = `
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://kit.fontawesome.com/d11723fac3.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/style.css">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicons/favicon-16x16.png">
+    <link rel="manifest" href="/img/favicons/site.webmanifest">
+
+    <meta property="og:title" content="Necsup - Future Defining Ecosystem">
+    <meta property="og:description" content="Building the greatest suite of apps ever created. Join the journey.">
+    <meta property="og:image" content="/img/stock/oslo-lake.jpg">
+    <meta property="og:url" content="https://necsup.com">
+`;
